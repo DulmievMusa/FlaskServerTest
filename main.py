1dd6b26ae5a6b3bd00c2305ed1d302bbac5bdbe0
Musa Dulmiev <musadulmiev@gmail.com>
2023-03-12 11:24:26 +0300

    first test

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, url_for
+from waitress import serve
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return f'''<html>
+    Привет от приложения Flask
+    <br>
+    <img src="{url_for('static', filename='smeshariki.png')}"
+    </html>
+    '''
+
+
+if __name__ == '__main__':
+    # app.run(port=8080, host='127.0.0.1')
+    serve(app, host='127.0.0.1', port=8080)
